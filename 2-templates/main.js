7c69a3a2822c145f9a3026d5041b446593db3a6f
Zac Allen <zac.allen@outlook.com>
2021-06-05 19:02:09 +0200

    added templates and directive for using html elements in vue instance

--- 2-templates/main.js
+++ 2-templates/main.js
@@ -4,11 +4,13 @@ let app = new Vue({
     //options object
     el: '#app',
     data: {
+        title: 'My Portfolio',
+        //will not work, as you need to use directives
+        titleHTML: `Zac Allen's <span class='badge'>Portfolio</span>`,
         alert: 'This is an alert message!',
         projects: [
             {title: 'Portfolio', desc: 'Lorem ipum'},
             {title: 'Twitter clone', desc: 'Lorem ipum'}
         ]
     }
-
-});
+}); 
